d66f93b34a10ec744bacfd80a361bfbb8606a6f1
Hassan Al-Najjar <hassan.an.najjar@gmail.com>
2022-01-03 16:21:19 +0200

    updated fonts size

--- components/Typography/style.js
+++ components/Typography/style.js
@@ -3,10 +3,10 @@ import { RFValue } from '../../utils/constants';
 
 const styles = StyleSheet.create({
   h1Style: { fontSize: RFValue(40) },
-  h2Style: { fontSize: RFValue(30) },
+  h2Style: { fontSize: RFValue(28) },
   h3Style: { fontSize: RFValue(20) },
   h4Style: { fontSize: RFValue(17) },
-  h5Style: { fontSize: RFValue(14) },
+  h5Style: { fontSize: RFValue(13) },
   h6Style: { fontSize: RFValue(11) },
   simpleText: { fontSize: RFValue(25) },
   inputStyle: { fontSize: RFValue(14) },
