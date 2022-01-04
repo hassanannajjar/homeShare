223d6f05fe2777bc5064cbd7100201633ec640b6
Hassan Al-Najjar <hassan.an.najjar@gmail.com>
2022-01-04 06:48:06 +0200

    force rtl and force use mobile font

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
+import { StatusBar, Text, I18nManager } from 'react-native';
 import { isReadyRef, navigationRef } from './utils/rootNavigation';
 import DataContext from './utils/context';
 import Routes from './Routes';
 
-const App = () => (
-  <DataContext.Provider value={{}}>
-    <NavigationContainer
-      ref={navigationRef}
-      onReady={() => {
-        isReadyRef.current = true;
-      }}>
-      <Routes user="userData" />
-    </NavigationContainer>
-  </DataContext.Provider>
-);
+const App = () => {
+  useEffect(() => {
+    I18nManager.allowRTL(false);
+    I18nManager.forceRTL(false);
+    Text.defaultProps = Text.defaultProps || {};
+    // Ignore dynamic type scaling on iOS
+    Text.defaultProps.allowFontScaling = false;
+  });
+
+  return (
+    <>
+      <StatusBar backgroundColor="#FFFFFF" barStyle="dark-content" />
+
+      <DataContext.Provider value={{}}>
+        <NavigationContainer
+          ref={navigationRef}
+          onReady={() => {
+            isReadyRef.current = true;
+          }}>
+          <Routes user="userData" />
+        </NavigationContainer>
+      </DataContext.Provider>
+    </>
+  );
+};
 
 export default App;
